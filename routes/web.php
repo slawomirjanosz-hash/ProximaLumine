6fc2b35a00bc204d67d600c0d67079561e25e15b
Sławomir Janosz <D:\The Bat!>
2026-01-04 19:30:27 +0100

    fix: redirect logged-in users from / to /magazyn/sprawdz

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ Route::get('/test', function () {
 
 // STRONA STARTOWA
 Route::get('/', function () {
+    if (Auth::check()) {
+        return redirect('/magazyn/sprawdz');
+    }
     return view('welcome');
 });
 
