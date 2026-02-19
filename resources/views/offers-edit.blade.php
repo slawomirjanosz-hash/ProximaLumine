1a1bc87d6d8dd3bee65eaebeaeae738b21c4fa26
Sławomir Janosz <D:\The Bat!>
2026-02-19 14:21:29 +0100

    Fix section name persistence - move hidden input outside content div and add id attribute

--- resources/views/offers-edit.blade.php
+++ resources/views/offers-edit.blade.php
@@ -426,8 +426,8 @@
                                         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
                                     </button>
                                 </div>
+                                <input type="hidden" id="custom{{ $sectionIndex + 1 }}-name-input" name="custom_sections[{{ $sectionIndex + 1 }}][name]" value="{{ $customSection['name'] ?? '' }}">
                                 <div id="custom{{ $sectionIndex + 1 }}-content" class="p-4 hidden">
-                                    <input type="hidden" name="custom_sections[{{ $sectionIndex + 1 }}][name]" value="{{ $customSection['name'] ?? '' }}">
                                     <table class="w-full mb-4 text-xs">
                                         <thead>
                                             <tr class="bg-gray-100">
@@ -1042,8 +1042,8 @@
                         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
                     </button>
                 </div>
+                <input type="hidden" id="${sectionId}-name-input" name="custom_sections[${customSectionCounter}][name]" value="${escapeHtml(sectionName.trim())}">
                 <div id="${sectionId}-content" class="p-4 hidden">
-                    <input type="hidden" id="${sectionId}-name-input" name="custom_sections[${customSectionCounter}][name]" value="${escapeHtml(sectionName.trim())}">
                     <table class="w-full mb-4 text-xs">
                         <thead>
                             <tr class="bg-gray-100">
